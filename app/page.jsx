ffcc48b823199b20f5abf63f178b44ac8e42828c
pawansuthar <pawansuthar15372097023627153@gmail.com>
2025-03-21 17:05:53 +0530

    Add image upload functionality to HomeSearch component and integrate react-dropzone

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,15 @@
 import CarCard from "@/components/CarCard";
 import HomeSearch from "@/components/Home-search";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 import { Button } from "@/components/ui/button";
-import { featuredCars } from "@/lib/data";
-import { ChevronRight } from "lucide-react";
+import { bodyTypes, carMakes, faqItems, featuredCars } from "@/lib/data";
+import { Calendar, Car, ChevronRight, Shield } from "lucide-react";
+import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
@@ -24,7 +31,7 @@ export default function Home() {
         </div>
       </section>
 
-      {/*  */}
+      {/* Featured car */}
       <section className="py-12">
         <div className="container mx-auto px-4">
           <div className="flex justify-between items-end mb-8">
@@ -45,6 +52,146 @@ export default function Home() {
               return <CarCard key={car.id} car={car} />;
             })}
           </div>
+        </div>
+      </section>
+      {/* browse by makes */}
+      <section className="py-12 bg-gray-50">
+        <div className="container mx-auto px-4">
+          <div className="flex justify-between items-end mb-8">
+            <h2 className="text-2xl font-bold">Browse by Make</h2>
+            <Button
+              className="cursor-pointer flex items-center"
+              variant="ghost"
+              asChild
+            >
+              <Link href="/cars">
+                {" "}
+                View All <ChevronRight className="ml-1 h-4 w-4" />
+              </Link>
+            </Button>
+          </div>
+          <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4">
+            {carMakes.map((make) => {
+              return (
+                <Link
+                  key={make.name}
+                  className="bg-white rounded-lg  shadow p-4  text-center  cursor-pointer"
+                  href={`/cars?make=${make.name}`}
+                >
+                  <div className="h-16 w-auto mx-auto mb-2 relative">
+                    <Image
+                      src={make.image}
+                      alt={make.name}
+                      fill
+                      style={{ objectFit: "contain" }}
+                    />
+                  </div>
+                  <h3 className="font-medium">{make.name}</h3>
+                </Link>
+              );
+            })}
+          </div>
+        </div>
+      </section>
+      {/* why to choose */}
+      <section className="py-16">
+        <div className="container mx-auto px-4">
+          <h2 className="text-2xl font-bold text-center mb-12">
+            Why Choose ICON CARS
+          </h2>
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+            <div className="text-center">
+              <div className="bg-blue-100 text-blue-700 rounded-full w-16 h-16 flex items-center justify-center mx-auto mb-4">
+                <Car className="h-8 w-8" />
+              </div>
+              <h3 className="text-xl font-bold mb-2 ">Wide Selection</h3>
+              <p className="text-gray-600">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima,
+                quibusdam.
+              </p>
+            </div>
+            <div className="text-center">
+              <div className="bg-blue-100 text-blue-700 rounded-full w-16 h-16 flex items-center justify-center mx-auto mb-4">
+                <Calendar className="h-8 w-8" />
+              </div>
+              <h3 className="text-xl font-bold mb-2 ">Wide Selection</h3>
+              <p className="text-gray-600">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima,
+                quibusdam.
+              </p>
+            </div>
+            <div className="text-center">
+              <div className="bg-blue-100 text-blue-700 rounded-full w-16 h-16 flex items-center justify-center mx-auto mb-4">
+                <Shield className="h-8 w-8" />
+              </div>
+              <h3 className="text-xl font-bold mb-2 ">Wide Selection</h3>
+              <p className="text-gray-600">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima,
+                quibusdam.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* browse by body type */}
+      <section className="py-12 bg-gray-50">
+        <div className="container mx-auto px-4">
+          <div className="flex justify-between items-end mb-8">
+            <h2 className="text-2xl font-bold">Browse by Body</h2>
+            <Button
+              className="cursor-pointer flex items-center"
+              variant="ghost"
+              asChild
+            >
+              <Link href="/cars">
+                {" "}
+                View All <ChevronRight className="ml-1 h-4 w-4" />
+              </Link>
+            </Button>
+          </div>
+          <div className="grid grid-cols-2 md:grid-cols-4  gap-4">
+            {bodyTypes.map((make) => {
+              return (
+                <Link
+                  key={make.name}
+                  className="relative group cursor-pointer"
+                  href={`/cars?bodyType=${make.name}`}
+                >
+                  <div className="overflow-hidden rounded-lg flex justify-end h-28 mb-4 relative">
+                    <Image
+                      src={make.image}
+                      alt={make.name}
+                      fill
+                      className="object-cover group-hover:scale-105 transition duration-300"
+                    />
+                  </div>
+                  <div className="absolute inset-0 bg-gradient-to-t from-black/60 to-transparent rounded-lg flex items-end">
+                    <h3 className="text-white  text-xl font-bold pl-4 pb-2">
+                      {make.name}
+                    </h3>
+                  </div>
+                </Link>
+              );
+            })}
+          </div>
+        </div>
+      </section>
+      {/* accordians */}
+      <section className="py-12 bg-gray-50">
+        <div className=" px-4 container mx-auto">
+          <h2 className="text-2xl font-bold text-center mb-8">
+            Frequently Asked Questions
+          </h2>
+          <Accordion type="single" collapsible className="w-full">
+            {faqItems.map((faq, i) => {
+              return (
+                <AccordionItem key={i} value={`item-${i}`}>
+                  <AccordionTrigger>{faq.question}</AccordionTrigger>
+                  <AccordionContent>{faq.answer}</AccordionContent>
+                </AccordionItem>
+              );
+            })}
+          </Accordion>
         </div>
       </section>
     </div>
--- components/Home-search.jsx
+++ components/Home-search.jsx
@@ -1,9 +1,152 @@
-import React from 'react'
+"use client";
+import React, { useState } from "react";
+import { Input } from "./ui/input";
+import { Camera, Upload } from "lucide-react";
+import { Button } from "./ui/button";
+import { useDropzone } from "react-dropzone";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
 
 const HomeSearch = () => {
-  return (
-    <div>HomeSearch</div>
-  )
-}
+  const [serchterm, setserchterm] = useState("");
+  const [isimageserchactive, setisimageserchactive] = useState(false);
+  const [imagepreview, setimagepreview] = useState("");
+  const [searchimage, setsearchimage] = useState(null);
+  const [isuploading, setisuploading] = useState(false);
 
-export default HomeSearch
+  const router = useRouter();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!serchterm.trim()) {
+      toast.error("Please enter a search term");
+      return;
+    }
+    router.push(`/cars?search/${encodeURIComponent(serchterm)}`);
+  };
+  const handleImageSearch = async (e) => {
+    e.preventDefault();
+    if (!searchimage) {
+      toast.error("Please upload an image");
+      return;
+    }
+  };
+  const onDrop = (acceptedFiles) => {
+    const file = acceptedFiles[0];
+    if (file) {
+      if (file.size > 5 * 1024 * 1024) {
+        toast.error("Image Size Must be less than 5mb");
+        return;
+      }
+      setisuploading(true);
+      setsearchimage(file);
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setimagepreview(reader.result);
+        setisuploading(false);
+        toast.success("Image Uploaded");
+      };
+      reader.onerror = () => {
+        setisuploading(false);
+        toast.error("Error uploading image");
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+  const { getRootProps, getInputProps, isDragActive, isDragReject } =
+    useDropzone({
+      onDrop,
+      accept: {
+        "/image/*": [".jpg", ".jpeg", ".png"],
+      },
+      maxFiles: 1,
+    });
+  return (
+    <div>
+      <form action="" onSubmit={handleSubmit}>
+        <div className="relative flex items-center">
+          <Input
+            type="text"
+            placeholder="enter car, model to ai serch"
+            value={serchterm}
+            onChange={(e) => setserchterm(e.target.value)}
+            className="pl-10 pr-12 py-6 w-full rounded-full border-gray-300 bg-white/95 backdrop-blur-sm"
+          />
+          <div className="absolute right-[100px]">
+            <Camera
+              size={35}
+              onClick={() => setisimageserchactive(!isimageserchactive)}
+              className="cursor-pointer rounded-xl p-1.5"
+              style={{
+                background: isimageserchactive ? "black" : "",
+                color: isimageserchactive ? "white" : "",
+              }}
+            />
+          </div>
+          <Button
+            type="submit"
+            className="absolute right-2 rounded-full cursor-pointer"
+          >
+            Search
+          </Button>
+        </div>
+      </form>
+      {isimageserchactive && (
+        <div className="mt-4">
+          <form action="" onSubmit={handleImageSearch}>
+            <div className="border-2 border-dashed border-gray-300 rounded-3xl  p-6 text-center">
+              {imagepreview ? (
+                <div className="flex flex-col  items-center">
+                  <img
+                    src={imagepreview}
+                    alt="car preview"
+                    className="h-40 object-contain mb-4"
+                  />
+                  <Button
+                    className="cursor-pointer"
+                    variant="outline"
+                    onClick={() => {
+                      setsearchimage(null);
+                      setimagepreview("");
+                      toast.info("Image Removed");
+                    }}
+                  >
+                    Remove Image
+                  </Button>
+                </div>
+              ) : (
+                <div {...getRootProps()} className="cursor-pointer">
+                  <input {...getInputProps()} />
+                  <div className="flex flex-col items-center">
+                    <Upload className="h-12 w-12  text-gray-500 mb-2" />
+                    <p>
+                      {isDragActive && !isDragReject
+                        ? "Leave the file here to upload"
+                        : "Drag & Drop car image for search"}
+                    </p>
+                    {isDragReject && (
+                      <p className="text-red-500 mb-2 ">Invalid Image Type</p>
+                    )}
+                    <p className="text-gray-400 text-sm">
+                      Supports: JPG,PNG (max 5MB)
+                    </p>
+                  </div>
+                </div>
+              )}
+            </div>
+            {imagepreview && (
+              <Button
+                type="submit"
+                className="w-full mt-2 cursor-pointer"
+                disabled={isuploading}
+              >
+                {isuploading ? "Uploading..." : "Search whith this image"}
+              </Button>
+            )}
+          </form>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default HomeSearch;
